e90a93193543c4d7df4f042d14b00b7fd4a04bbb
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-04-11 23:24:02 +0530

    init

--- weather_now/urls.py
+++ weather_now/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render
+import requests
 
-urlpatterns = [
-    path('', views.index),  #the path for our index view
-]
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=a6e4d71ca1f45ad86b009d61f585'
+
+    city = 'Chennai'
+
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+    return render(request, 'weather/index.html') #returns the index.html template
