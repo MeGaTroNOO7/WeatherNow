318340072d28c2f5a80390b5d47d97788d47ff92
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-04-15 23:41:48 +0530

    init

--- weathernow/views.py
+++ weathernow/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=a6e4d71ca1f45ad86b009d61f58526f9'
+
+    city = 'Las Vegas'
+
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
     return render(request, 'weather/index.html') #returns the index.html template
-
-
