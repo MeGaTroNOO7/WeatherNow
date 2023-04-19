fb7a5f3dc4fddc0dd4ebd17e43c506dca45ad5cc
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-04-19 23:02:14 +0530

    init

--- weather_now/views.py
+++ weather_now/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'weather/index.html') #returns the index.html template
+    return render(request, 'weathernow/index.html') #returns the index.html template
